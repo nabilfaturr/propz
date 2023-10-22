671f0c2bfa5e60a67c0791c9b3595ba7f0eed4fe
nabilfaturr <nabilfaturr44@gmail.com>
2023-10-22 11:00:19 +0700

    create and styling header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/profile" element={<Profile/>}></Route>
-        <Route path="/signin" element={<SignIn/>}></Route>
-        <Route path="/signup" element={<SignUp/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 mx-auto shadow-md">
+      <div className="flex justify-between items-center mx-auto px-3 py-5 gap-3 max-w-[1200px]">
+        <Link to={"./"}>
+          <h1 className="text-lg xs:text-xl font-extrabold sm:text-2xl text-blue-900 cursor-pointer">
+            propertX
+          </h1>
+        </Link>
+        <form className="flex items-center px-4 bg-white rounded-lg">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent focus:outline-none rounded-xl py-2 w-32 xs:w-56 sm:w-64 md:w-80 lg:w-[36rem]"
+          />
+          <FaSearch className="text-slate-700" size={20} />
+        </form>
+        <div className="flex sm:gap-8 items-center">
+          <Link to={"./sign-in"}>
+            <h1 className="text-slate-700 hover:underline cursor-pointer text-md  sm:text-lg">
+              Sign In
+            </h1>
+          </Link>
+          <Link to={"./about"}>
+            <h1 className="hidden sm:inline-block text-slate-700 sm:text-lg hover:underline cursor-pointer">
+              About
+            </h1>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
