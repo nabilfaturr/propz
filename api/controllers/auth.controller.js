3c20d99997915a5d35fa16bbc42f741a59ba5d6c
nabilfaturr <nabilfaturr44@gmail.com>
2023-10-24 15:06:19 +0700

    create sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "./../models/user.model.js";
 import bcrypsjs from "bcryptjs";
+import { errorHandler } from "./../utils/error.util.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -13,9 +15,39 @@ export const signup = async (req, res, next) => {
     const newUser = new User({ username, password: hashedPassword, email });
     await newUser.save();
 
-    res.status(201).json("User Created succesfully")
-    
+    res.status(201).json("User Created succesfully");
   } catch (error) {
-    next(error)
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found!"));
+    }
+
+    const validPassword = await bcrypsjs.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "Invalid Credential!"));
+    }
+
+    console.log(validUser._doc);
+
+    const {password: pass, ...rest} = await validUser._doc;
+
+    const token = jwt.sign(
+      validUser._id.toString(),
+      process.env.JWT_SECRET_KEY
+    );
+
+    res
+      .cookie("access-token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
