888b475c8862272c64a156b38e9f1a8b44227064
nabilfaturr <nabilfaturr44@gmail.com>
2023-11-09 19:10:08 +0700

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -12,8 +12,6 @@ export const createListing = async (req, res, next) => {
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
-  console.log(listing);
-
   if (!listing) {
     return next(404, "Listing not found!");
   }
@@ -25,6 +23,31 @@ export const deleteListing = async (req, res, next) => {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing has been deleted!");
   } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(404, "Listing not found!");
+  }
+
+  if (req.user !== listing.userRef) {
+    return next(401, "You can only delete your own listing!");
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    console.erorr(error);
     next(error);
   }
 };
